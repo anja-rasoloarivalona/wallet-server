c61725cea04c18153235dce12525d723203943b7
anjarasoloarivalona <anja@bizbizshare.com>
2021-02-28 13:36:13 -0500

    Upated env varivalbles

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,3 +1,3 @@
 import Sequelize from 'sequelize'
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+const sequelize = new Sequelize(process.env.JAWSDB_URL);
 export default sequelize;
